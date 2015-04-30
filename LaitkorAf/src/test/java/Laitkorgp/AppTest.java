1af8de813f89fecdb12d51fe418123faf4c06377
Sharikh Abbas <shariq.abbas@laitkor.com>
2015-04-30 18:35:17 +0530

    First

---
+++ LaitkorAf/src/test/java/Laitkorgp/AppTest.java
@@ -0,0 +1,16 @@
+package Laitkorgp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void samtest(){
+		WebDriver dri=new FirefoxDriver();
+		dri.get("http://www.google.com");
+		}
+
+}
