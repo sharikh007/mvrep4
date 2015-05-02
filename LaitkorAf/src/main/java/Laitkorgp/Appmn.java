dc482035e863f8151747ba4c5f0ffb2cc245f5e7
Sharikh Abbas <shariq.abbas@laitkor.com>
2015-05-02 13:01:24 +0530

    one more commit

---
+++ LaitkorAf/src/main/java/Laitkorgp/Appmn.java
@@ -0,0 +1,20 @@
+package Laitkorgp;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+
+public class Appmn
+{
+public static void main(String[] args) throws IOException,InterruptedException {
+List<String> suitesList = new ArrayList<String>();
+TestListenerAdapter tla = new TestListenerAdapter();
+TestNG testng = new TestNG();
+suitesList.add(Appmn.class.getResource("testng.xml").getFile());
+testng.setTestSuites(suitesList);
+testng.addListener(tla);
+testng.run();
+}
+}
--- LaitkorAf/src/test/java/Laitkorgp/AppTest.java
+++ LaitkorAf/src/test/java/Laitkorgp/AppTest.java
@@ -1,16 +1,32 @@
 package Laitkorgp;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class AppTest {
-	
-	@Test
-	public void samtest(){
-		WebDriver dri=new FirefoxDriver();
-		dri.get("http://www.yahoo.com");
+	WebDriver dri;
+	@BeforeTest
+	@Parameters("browser")
+	public void bef(String browser){
+		if(browser.equalsIgnoreCase("firefox")){
+			dri=new FirefoxDriver();
 		}
-
+		else if(browser.equalsIgnoreCase("chrome")){
+			System.getProperty("webdriver.chrome.driver","/path/to/chromedriver");
+			dri=new ChromeDriver();
+		}
+		}
+ @Test(testName="testone", priority=1)
+	public void paralleltetsone() {
+	dri.get("http://www.yahoo.com");
+		}
+ @Test(testName="testtwo",priority=2)
+	public void paralleltesttwo() {
+	dri.get("http://www.google.com");
+		}
 }
